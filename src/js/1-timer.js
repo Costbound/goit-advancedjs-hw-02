1b9f5d8406a671c8de3a7b0a500cb86475256334
Dmytro Popovskyi <popovskiy96@gmail.com>
2025-05-14 01:04:57 +0200

    done

---
+++ src/js/1-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const dayOut = document.querySelector('[data-days]');
+const hourOut = document.querySelector('[data-hours]');
+const minOut = document.querySelector('[data-minutes]');
+const secOut = document.querySelector('[data-seconds]');
+const startBtn = document.querySelector('button');
+
+let selectedDate;
+startBtn.setAttribute('disabled', '');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose([selectedDates]) {
+    if (selectedDates < options.defaultDate) {
+      startBtn.setAttribute('disabled', '');
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        backgroundColor: 'rgb(236, 56, 56)',
+        messageColor: '#FFF',
+        position: 'center',
+      });
+    } else {
+      startBtn.removeAttribute('disabled');
+      selectedDate = selectedDates.getTime();
+    }
+  },
+};
+const dateInput = flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+startBtn.onclick = () => {
+  if (selectedDate > Date.now()) {
+    const calcTimer = () => {
+      outputsUpdate(
+        [dayOut, hourOut, minOut, secOut],
+        convertMs(selectedDate - Date.now())
+      );
+      if (selectedDate - 1000 < Date.now() && secOut.textContent == '00')
+        clearInterval(timer);
+    };
+    calcTimer();
+    const timer = setInterval(calcTimer, 1000);
+    dateInput.input.setAttribute('disabled', '');
+    startBtn.setAttribute('disabled', '');
+    startBtn.dataset.start = 'started';
+  } else {
+    iziToast.show({
+      message: 'Please choose a date in the future',
+      backgroundColor: 'rgb(236, 56, 56)',
+      messageColor: '#FFF',
+      position: 'center',
+    });
+    startBtn.setAttribute('disabled', '');
+  }
+};
+
+const outputUpdate = (output, time) => {
+  output.textContent = time.toString().padStart(2, '0');
+};
+const outputsUpdate = (
+  [dayOut, hourOut, minOut, secOut],
+  { days, hours, minutes, seconds }
+) => {
+  outputUpdate(dayOut, days);
+  outputUpdate(hourOut, hours);
+  outputUpdate(minOut, minutes);
+  outputUpdate(secOut, seconds);
+};
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('form');
+const msInput = form.elements.delay;
+const fullfieldRadio = form.elements.state[0];
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  createPromise(Number(msInput.value), fullfieldRadio.checked);
+});
+
+function createPromise(delay, shouldResolve) {
+  if (delay > 0) {
+    const promise = new Promise((res, rej) => {
+      setTimeout(() => {
+        if (shouldResolve) res(`✅ Fulfilled promise in ${delay}ms`);
+        else rej(`❌ Rejected promise in ${delay}ms`);
+      }, delay);
+    });
+    promise
+      .then(value => {
+        iziToast.show({
+          message: value,
+          backgroundColor: 'rgba(82, 223, 79, 0.3)',
+          position: 'topRight',
+        });
+      })
+      .catch(value => {
+        iziToast.show({
+          message: value,
+          backgroundColor: 'rgba(223, 79, 79, 0.3)',
+          position: 'topRight',
+        });
+      });
+  } else {
+    iziToast.show({
+      message: 'Value must be more than 0',
+      backgroundColor: 'yellow',
+      position: 'topRight',
+    });
+  }
+}
